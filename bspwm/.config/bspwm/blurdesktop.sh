9b565328fe2d8efa85899b61fc78fac0175a5528
Robert Washbourne <rawashbourne@gmail.com>
2017-04-28 09:45:22 -0500

    Update blurdesktop.sh

--- bspwm/.config/bspwm/blurdesktop.sh
+++ bspwm/.config/bspwm/blurdesktop.sh
@@ -6,13 +6,13 @@ while read line
 do
 	if [[ "$line" ==  *FI* ]]; then
 		if [ "$blur" = true ]; then
-			feh --bg-scale "/home/robert/Pictures/wallfab.jpg"
+			feh --bg-scale "~/Pictures/wallfab.jpg" #non blurred wall
 			blur=false
 		fi
 	else
 		if [ "$blur" = false ]; then
-			feh --bg-scale "/home/robert/Pictures/wallblur.jpg"
+			feh --bg-scale "~/Pictures/wallblur.jpg" #blurred wall
 			blur=true
 		fi
 	fi
-done < <(bspc subscribe)
+done < <(bspc subscribe) #subscribe to bspc (get notified of changes in windows)
